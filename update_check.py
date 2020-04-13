2217277ea57b8391b2e6596206524130fe68e6a3
Haruka0522 <harukapc1@gmail.com>
2020-04-14 00:26:49 +0900

    過去の記事をSpreadSheetに保存するように変更

--- update_check.py
+++ update_check.py
@@ -1,6 +1,8 @@
 import os
 from requests_html import HTMLSession
 import requests
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 
 # 環境変数
 LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
@@ -16,19 +18,27 @@ def PushMessage():
     return response
 
 session = HTMLSession()
-
 r = session.get("https://sasayama-jh.sasayama.jp/")
-
 latest_article = r.html.find("article")[0].text
 
-past_article = ""
-try:
-    with open("tmp/article_log.txt") as f:
-        past_article = f.read()
-except:
-    with open("tmp/article_log.txt","w"):
-        pass
-with open("tmp/article_log.txt", mode="w") as f:
-    f.write(latest_article)
+scope = ['https://spreadsheets.google.com/feeds',
+         'https://www.googleapis.com/auth/drive']
+credential = {
+                "type": "service_account",
+                "project_id": os.environ['SHEET_PROJECT_ID'],
+                "private_key_id": os.environ['SHEET_PRIVATE_KEY_ID'],
+                "private_key": os.environ['SHEET_PRIVATE_KEY'],
+                "client_email": os.environ['SHEET_CLIENT_EMAIL'],
+                "client_id": os.environ['SHEET_CLIENT_ID'],
+                "auth_uri": "https://accounts.google.com/o/oauth2/auth",
+                "token_uri": "https://oauth2.googleapis.com/token",
+                "auth_provider_x509_cert_url": "https://www.googleapis.com/oauth2/v1/certs",
+                "client_x509_cert_url":  os.environ['SHEET_CLIENT_X509_CERT_URL']
+             }
+credentials = ServiceAccountCredentials.from_json_keyfile_dict(credential, scope)
+gc = gspread.authorize(credentials)
+wks = gc.open("past_article").sheet1
+past_article = str(wks.acell("A1").value)
+wks.update_acell("A1",latest_article)
 if latest_article != past_article:
     PushMessage()
