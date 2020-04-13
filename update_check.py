04ef1ca31fae6d2d8050b5cbaad3156e5b7a63e7
Haruka0522 <harukapc1@gmail.com>
2020-04-13 22:59:14 +0900

    更新されているかチェックするスクリプトを追加

---
+++ update_check.py
@@ -0,0 +1,29 @@
+import os
+from requests_html import HTMLSession
+import requests
+
+# 環境変数
+LINE_CHANNEL_ACCESS_TOKEN = os.environ["LINE_CHANNEL_ACCESS_TOKEN"]
+
+def PushMessage():
+    Authorization = "Bearer {"+LINE_CHANNEL_ACCESS_TOKEN+"}"
+    headers = {
+        'Content-Type': 'application/json',
+        'Authorization': Authorization,
+    }
+    data = '{\n    "messages":[\n        {\n            "type":"text",\n            "text":"Update the homepage https://sasayama-jh.sasayama.jp/"\n        }\n    ]\n}'
+    response = requests.post('https://api.line.me/v2/bot/message/broadcast', headers=headers, data=data)
+    return response
+
+session = HTMLSession()
+
+r = session.get("https://sasayama-jh.sasayama.jp/")
+
+latest_article = r.html.find("article")[0].text
+
+with open("./article_log.txt") as f:
+    past_article = f.read()
+with open("./article_log.txt", mode="w") as f:
+    f.write(latest_article)
+if latest_article != past_article:
+    PushMessage()
